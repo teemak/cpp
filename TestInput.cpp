7098bf9fa4e05df930d4e94e6efa31991d25f9ce
teemak <bunth667@gmail.com>
2021-01-10 16:35:55 -0500

    terminal command not taking input

---
+++ TestInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string FirstName, LastName;
+    
+    cout << "Enter your given name: ";
+    cin >> FirstName;
+    cout << "Enter your family name: ";
+    cin >> LastName;
+
+    cout << "Your full name is " << FirstName + LastName;
+
+    return 0;
+}
--- triplex.cpp
+++ triplex.cpp
@@ -7,23 +7,33 @@ int main() {
     std::cout << "You are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
-    // Expression Statements - because of semicolon
 
     // Declaration Statements - declaring variables;
-    const int a = 8;
-    int b = 2;
-    int c = 4;
-    b = 1;
-    int sum = a + b + c;
-    int product = a * b * c;
-    // Declaration Statements - declaring variables;
+    // Naming standard is uppercase camel case
+    const int CodeA = 8;
+    const int CodeB = 18;
+    const int CodeC = 80;
 
+    int sum = CodeA + CodeB + CodeC;
+    int product = CodeA * CodeB * CodeC;
+
+    /*  multi
+        line
+        comment
+    */
+
+    std::cout << std::endl << std::endl;
+    std::cout << "Three numbers make up the code." << std::endl;
+    std::cout <<  "The sum of numbers is " << sum << "." << std::endl;
+    std::cout <<  "The product equals " << product << "." << std::endl;
     std::cout << std::endl;
-    std::cout <<  sum;
-    std::cout << std::endl;
-    std::cout <<  product;
+
+    int PlayerGuess;
+    std::cin >> PlayerGuess;
+    std::cout << "You entered: " << PlayerGuess;
+    
+    std::cout << "TEST TEST";
 
     // Return Statement
     return 0; 
 }
-// Main Function
