533d7910fd40782259782838878786d716389fa2
teemak <bunth667@gmail.com>
2021-01-10 15:26:35 -0500

    types of statements

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//using namespace std;
+// std namespace
+// :: scope operator
+// << outputs string
+int main() {
+    std::cout << "Not using namespace std";
+    // using namespace allows next line to compile
+    //cout << "Hello World!";
+    return 0;
+}
--- triplex.cpp
+++ triplex.cpp
@@ -1,7 +1,29 @@
+// Preprocessor Directive
 #include <iostream>
-using namespace std;
 
+// Main Function
 int main() {
-    cout << "Hello World!";
-    return 0;
+    // Expression Statements
+    std::cout << "You are a secret agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue...";
+    // Expression Statements - because of semicolon
+
+    // Declaration Statements - declaring variables;
+    const int a = 8;
+    int b = 2;
+    int c = 4;
+    b = 1;
+    int sum = a + b + c;
+    int product = a * b * c;
+    // Declaration Statements - declaring variables;
+
+    std::cout << std::endl;
+    std::cout <<  sum;
+    std::cout << std::endl;
+    std::cout <<  product;
+
+    // Return Statement
+    return 0; 
 }
+// Main Function
